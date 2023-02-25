bf08c33a3cfedb3830f496fbd01a0a8c6b7f86b0
brxshncy <ggkidsbruce619@gmail.com>
2023-02-25 12:18:31 +0800

    set up react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { NavBar } from "./components/common/NavBar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { LandingPage } from "./pages/LandingPage/LandingPage";
+import { HomePage } from "./pages/HomPage/HomePage";
+import { WeatherPage } from "./pages/WeatherPage/WeatherPage";
 
 export const App = () => {
     return (
-        <div>
+        <Router>
             <NavBar />
-        </div>
+            <Routes>
+                <Route path='/' element={<LandingPage />} />
+                <Route path='/home' element={<HomePage />} />
+                <Route path='/weather' element={<WeatherPage />} />
+            </Routes>
+        </Router>
     );
 };
